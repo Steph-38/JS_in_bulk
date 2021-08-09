7a92151958d6ba04cd434888924c1f306d4980a4
Steph-38 <44949858+Steph-38@users.noreply.github.com>
2021-08-09 12:08:11 +0200

    Update email.js in english

--- email/email.js
+++ email/email.js
@@ -1,11 +1,11 @@
 function checkEmail() {
-  let email = prompt('Saisie de l\'email')
+  let email = prompt('Please enter the email')
   let tag = document.getElementById('email')
   let myRegex = /^[a-zA-Z0-9._-]+@[a-z0-9_-]+\.[a-z]{2,6}$/
 
   if (myRegex.test(email)) {
-    tag.innerHTML = "le mail est valable"
+    tag.innerHTML = "The email is valid"
   } else {
-    tag.innerHTML = "le mail n'est pas valable"
+    tag.innerHTML = "The email is invalid"
   }
 }
